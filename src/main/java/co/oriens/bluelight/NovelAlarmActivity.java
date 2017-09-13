156fd8cd069ca312d9101e70b3dd19f2a1f1121a
doguTheCreator <ddu@oriens.co>
2017-09-13 11:19:38 +0300

    New alarm activity created

---
+++ src/main/java/co/oriens/bluelight/NovelAlarmActivity.java
@@ -0,0 +1,13 @@
+package co.oriens.bluelight;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NovelAlarmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_novel_alarm);
+    }
+}
