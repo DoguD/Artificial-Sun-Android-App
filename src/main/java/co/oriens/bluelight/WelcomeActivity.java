5162d663a82ae810e4d4e9b7ecfcfd782d840b48
doguTheCreator <ddu@oriens.co>
2017-09-16 17:42:12 +0300

    Directly opens AlarmsListActivity after intro slides. Tab Activity and related material is purged.

--- src/main/java/co/oriens/bluelight/WelcomeActivity.java
+++ src/main/java/co/oriens/bluelight/WelcomeActivity.java
@@ -20,6 +20,8 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import co.oriens.bluelight.presenter.AlarmsListActivity;
+
 public class WelcomeActivity extends AppCompatActivity {
 
     //Değişkenler oluşturuluyor
@@ -123,7 +125,7 @@ public class WelcomeActivity extends AppCompatActivity {
     //Tanıtımdan sonra hangi aktivitenin açılacağını kontrol eden method
     private void launchMainActivity() {
         prefManager.setFirstTimeLaunch(false);
-        startActivity(new Intent(WelcomeActivity.this, TabActivity.class));
+        startActivity(new Intent(WelcomeActivity.this, AlarmsListActivity.class));
         finish();
     }
 
